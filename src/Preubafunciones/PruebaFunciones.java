423a1f2662289fe3806fec3c6a57bfd107cab163
ifadoua2 <f647379@gmail.com>
2023-03-02 12:52:07 +0100

    funccions

---
+++ src/Preubafunciones/PruebaFunciones.java
@@ -0,0 +1,9 @@
+package Preubafunciones;
+
+import com.factoriaf5.FuncionesYParametros;
+
+public class PruebaFunciones {
+   public static  void main (String[]args){
+       FuncionesYParametros.holaMundo();
+   }
+}
--- src/com/factoriaf5/FuncionesYParametros.java
+++ src/com/factoriaf5/FuncionesYParametros.java
@@ -1,4 +1,32 @@
 package com.factoriaf5;
 
 public class FuncionesYParametros {
+    public static void main(String[]args) {
+
+        // holaMundo();
+        // holaMundo();
+
+       //holaMundo(name:"Alan");
+        //holaMundo(name"Roberto");
+
+        String hola = devolverHola();
+        System.out.println(hola);
+
+
+    }
+public static void holaMundo(){
+    System.out.println("Hola mundo");
+    System.out.println("Hola mundo");
+
+    }
+
+    private static void holaMundo(String nam){
+        System.out.println("Hola "+ nam);
+    }
+    private static String devolverhola(){
+        return"Hola";
+    }
+    private static int sum(int num1,int num2){
+
+    }
 }
